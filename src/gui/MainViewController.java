e5a2fea9cb0d993e8923bfe9a482fe044fdffb54
Luiz Felipe Mecina Greijo <lfelipe1993@live.com>
2020-04-07 13:53:57 -0300

    Department service

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -34,7 +35,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
@@ -66,6 +67,34 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			//adiciona o newVBOX conteudo
 			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private synchronized void loadView2(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			//pega referencia do scrollpanel do main
+			Scene mainScene = Main.getMainScene();
+			//pega o Vbox do scrollpane
+			VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent();
+			
+			//seleciona o menu dentro do vbox para preserva-lo
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//limpa o vbox
+			mainVBox.getChildren().clear();
+			//adiciona o menu
+			mainVBox.getChildren().add(mainMenu);
+			//adiciona o newVBOX conteudo
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
